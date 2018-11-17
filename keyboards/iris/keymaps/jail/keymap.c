9d2034fc5a0bafa0c4ad7e778615cd0cb66b0f64
Nithya Hariharan <nits@nithyas-macbook.lan>
2018-11-17 14:51:03 +0800

    Removed KC

--- keyboards/iris/keymaps/jail/keymap.c
+++ keyboards/iris/keymaps/jail/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-//extern keymap_config_t keymap_config;
+extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
 #define _LOWER 1
@@ -12,30 +12,17 @@ enum custom_keycodes {
   RAISE,
 };
 
-//#define KC_ KC_TRNS
+#define KC_       KC_TRNS
+#define KC_LOWR   LT(_LOWER, KC_F)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     KC_1,KC_2,KC_3,KC_4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,DEL ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LSFT, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LCTL, Z  , X  , C  , V  , B  ,HOME,     END , N  , M  ,COMM,DOT ,SLSH,RSFT,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       ADJT,LOWR, SPC,         SCET,RASE, ENT
-  //                  `----+----+----'        `----+----+----'
-  ),
-
-	[_QWERTY] = LAYOUT(
-KC_1, KC_2, KC_3, KC_4, KC_5, KC_ESC, KC_PGUP, KC_6, KC_7, KC_8, KC_9, KC_0,
-KC_Q, KC_W, KC_E, KC_R, KC_T, KC_CAPS, KC_PGDN, KC_Y, KC_U, KC_I, KC_O, KC_P,
-KC_A, KC_S, KC_D, LT(_LOWER, KC_F), KC_G, KC_HOME, KC_END, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
-KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL, KC_UNDO, KC_AGIN, KC_BSPC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-KC_LALT, KC_SPC, KC_LSFT, KC_LCTL, KC_SPC, KC_ENT),
-	[_LOWER] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_VOLU, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_VOLD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-	[_RAISE] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SLSH, KC_BSLS, KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_EQL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO)
+	[_QWERTY] = LAYOUT_kc(
+1, 2, 3, 4, 5, ESC, PGUP, 6, 7, 8, 9, 0,
+Q, W, E, R, T, CAPS, PGDN, Y, U, I, O, P,
+A, S, D, LOWR, G, HOME, END, H, J, K, L, SCLN,
+Z, X, C, V, B, DEL, UNDO, AGIN, BSPC, N, M, COMM, DOT, SLSH,
+LALT, SPC, LSFT, LCTL, SPC, ENT),
+	[_LOWER] = LAYOUT_kc(F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, TRNS, NO, NO, VOLU, LEFT, DOWN, UP, RGHT, NO, NO, NO, NO, NO, NO, NO, TRNS, NO, VOLD, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO),
+	[_RAISE] = LAYOUT_kc(NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, SLSH, BSLS, LCBR, RCBR, NO, NO, NO, NO, NO, NO, NO, NO, LBRC, RBRC, LPRN, RPRN, NO, NO, NO, NO, NO, NO, NO, NO, NO, MINS, EQL, NO, NO, NO, NO, NO, TRNS, NO, NO, NO)
 };

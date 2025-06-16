eb67eb24f4f2b281a7f8da8f4d7619292454d682
Vijay Lakshmanan <vijay.ellu@gmail.com>
2025-06-16 09:19:46 -0400

    Added a third layer, plus send string

--- keyboards/sofle_choc/keymaps/craftingkeebs/keymap.c
+++ keyboards/sofle_choc/keymaps/craftingkeebs/keymap.c
@@ -16,13 +16,19 @@
 #include QMK_KEYBOARD_H
 
 enum sofle_layers {
-  _QWERTY,
-  _LOWER,
-  _RAISE
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _BOTH
+};
+
+enum planck_keycodes {
+  PASS = SAFE_RANGE
 };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define BOTH MO(_BOTH)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -47,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,   KC_HOME,                      KC_END,  KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,
     KC_A,   KC_S,    KC_D,    KC_F,    KC_G,   KC_TAB,                       KC_CAPS,   KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN,
     KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_DEL,    KC_MUTE,  KC_MPLY, KC_BSPC,  KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH,
-                      KC_LCTL, KC_LGUI,  KC_SPC,  LOWER, KC_ENT,     KC_RALT, RAISE, KC_ENT,  KC_LSFT, KC_RCTL
+                      KC_LCTL, KC_LGUI,  KC_SPC,  LOWER, KC_LALT,     KC_RALT, RAISE, KC_ENT,  KC_LSFT, KC_RCTL
 ),
 
  /*
@@ -94,6 +100,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,                      _______, _______, KC_SLSH, KC_NUBS,       LSFT(KC_LBRC), LSFT(KC_RBRC),
     _______, _______, _______, _______, _______, _______,                      _______, _______, KC_LBRC, KC_RBRC,       LSFT(KC_9),    LSFT(KC_0),
     KC_LSFT, _______, _______, _______, _______, _______,  _______,   _______, _______, KC_MINS, KC_EQL,  LSFT(KC_BSLS), KC_DOT,        KC_QUOT,
+                      _______, _______, _______,    BOTH, _______,    _______, _______,  _______,  _______, _______
+),
+
+/*
+ * BOTH
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    |      |      |  /   | \or| |  {   |  }   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      | PASS |      |      |-------.    ,-------|      |      |  [/{ | ]/}  |  (   |  )   |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * | Shift|      |      |      |      |      |-------|    |-------|      |  -/_ |  =/+ |   |  |  ./> | '/"" |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            |      |      |      |      | /       /       \      \  |      |      |      |      |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+[_BOTH] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______,  _______, _______,
+    _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______,  _______, _______,
+    _______, _______, _______,    PASS, _______, _______,                      _______, _______, _______, _______,  _______, _______,
+    _______, _______, _______, _______, _______, _______,  _______,   _______, _______, _______, _______, _______,  _______, _______,
                       _______, _______, _______, _______, _______,    _______, _______,  _______,  _______, _______
 )
 
@@ -101,8 +130,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-    [2] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
 };
 #endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case PASS:
+      if (record->event.pressed) {
+        SEND_STRING();
+      } else {
+      }
+      break;
+  }
+  return true;
+};
